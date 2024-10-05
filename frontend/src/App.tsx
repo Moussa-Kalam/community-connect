bd7d6525c677b282696004fc7283e5c64f89e108
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-10-05 21:12:37 +0200

    chore: initial setup for the frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <p className="text-4xl text-green-400 bg-black h-screen flex items-center justify-center">
+      Welcome to Community Connect
+    </p>
+  );
+}
