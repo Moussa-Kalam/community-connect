9cbe0a0a9a1646114328ce626e4ace30d6a3cd80
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-10-15 13:23:18 +0200

    feat: implement the password hashing functionality

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
 
 @Module({
   imports: [
@@ -26,6 +27,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }),
 
     UsersModule,
+
+    IamModule,
   ],
   controllers: [AppController],
   providers: [AppService],
