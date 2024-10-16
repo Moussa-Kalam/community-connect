a440aacaf780ad91086ba2a460af3ff2e0039d47
Moussa-Kalam <moussakalamamzat@gmail.com>
2024-10-16 22:19:37 +0200

    refactor the Sign up page to include only needed fields

--- frontend/src/components/ui/Stepper.tsx
+++ frontend/src/components/ui/Stepper.tsx
@@ -1,22 +1,21 @@
 export default function Stepper({ step }: { step: number }) {
   return (
     <ul className="steps steps-horizontal">
-      <li className={`step ${step >= 1 ? "step-primary" : ""}`}>
+      <li className={`step px-6 ${step >= 1 ? "step-primary" : ""}`}>
         Personal Information
       </li>
+
       <li className={`step ${step >= 2 ? "step-primary" : ""}`}>
-        Account Details
-      </li>
-      <li className={`step ${step >= 3 ? "step-primary" : ""}`}>
         Contact Information
       </li>
-      <li className={`step ${step >= 4 ? "step-primary" : ""}`}>
-        Account Type
+
+      <li className={`step ${step >= 3 ? "step-primary" : ""}`}>
+        Account Details
       </li>
-      <li className={`step ${step >= 5 ? "step-primary" : ""}`}>
+
+      <li className={`step ${step >= 4 ? "step-primary" : ""}`}>
         Account Security
       </li>
-      <li className={`step ${step >= 6 ? "step-primary" : ""}`}>Bio</li>
     </ul>
   );
 }
--- frontend/src/schemas/loginSchema.ts
+++ frontend/src/schemas/loginSchema.ts
@@ -1,10 +1,10 @@
-import {z} from "zod";
-import {AccountDetailsSchema} from "./baseSchema.ts";
+import { z } from "zod";
+import { PersonalInfoSchema } from "./baseSchema.ts";
 
 export const LoginSchema = z.object({
-    username: AccountDetailsSchema.shape.username,
-    password: z
-        .string({required_error: "Password is required"})
-        .trim()
-        .min(6, {message: "Password must be at least 6 characters long"}),
+  username: PersonalInfoSchema.shape.username,
+  password: z
+    .string({ required_error: "Password is required" })
+    .trim()
+    .min(6, { message: "Password must be at least 6 characters long" }),
 });
